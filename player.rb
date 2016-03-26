2a22902f5dde25d57aa80baf80225f1bfb8a5e68
mr.The <me@mrthe.name>
2016-03-26 10:53:08 +0200

    set minimum * 2

--- player.rb
+++ player.rb
@@ -4,7 +4,10 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    game_state["minimum_raise"]
+    game_state["minimum_raise"] * 2
+
+  rescue
+    rand(500)
   end
 
   def showdown(game_state)
